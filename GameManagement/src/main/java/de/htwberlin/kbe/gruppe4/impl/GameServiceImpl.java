54772d9a283ce5c9e7d3e7cbeca2f693877f4ff4
NilsLeo <nilstleo01@gmail.com>
2023-01-28 23:01:03 +0100

    refactor der privaten felder von gameServiceImpl -> Game

--- GameManagement/src/main/java/de/htwberlin/kbe/gruppe4/impl/GameServiceImpl.java
+++ GameManagement/src/main/java/de/htwberlin/kbe/gruppe4/impl/GameServiceImpl.java
@@ -14,19 +14,11 @@ public class GameServiceImpl implements GameService {
     private DeckService deckService;
     RulesService rulesService;
     PlayerService playerService;
-    private final Deck deck;
-    private List<Card> table;
-    private final Rules rules;
-    private int currentPlayer;
     private Game game;
 
     @Inject
     public GameServiceImpl(DeckService deckService, RulesService rulesService,
             PlayerService playerService) {
-        this.deck = new Deck();
-        this.table = new ArrayList<>();
-        this.rules = new Rules();
-        this.currentPlayer = 0;
         this.deckService = deckService;
         this.rulesService = rulesService;
         this.playerService = playerService;
@@ -42,9 +34,9 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public void setRules(boolean drawTwoOnSeven, boolean chooseSuitOnJack, boolean reverseOnAce) {
-        rules.setDrawTwoOnSeven(drawTwoOnSeven);
-        rules.setChooseSuitOnJack(chooseSuitOnJack);
-        rules.setReverseOnAce(reverseOnAce);
+        game.getRules().setDrawTwoOnSeven(drawTwoOnSeven);
+        game.getRules().setChooseSuitOnJack(chooseSuitOnJack);
+        game.getRules().setReverseOnAce(reverseOnAce);
         logger.info("Rules for the game have been set: draw two on seven = " + drawTwoOnSeven +
                 ", choose suit on jack = " + chooseSuitOnJack +
                 ", reverse on ace = " + reverseOnAce);
@@ -52,23 +44,23 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public void startGame() {
-        deckService.shuffle(deck);
-        table.add(deckService.deal(deck));
+        deckService.shuffle(game.getDeck());
+        game.getTable().add(deckService.deal(game.getDeck()));
         for (Player player : game.getPlayers()) {
-            playerService.dealHand(player, deck);
+            playerService.dealHand(player, game.getDeck());
         }
         logger.info("Game started");
     }
 
     @Override
     public Card getLeadCard() {
-        return table.get(table.size() - 1);
+        return game.getTable().get(game.getTable().size() - 1);
 
     }
 
     @Override
     public boolean hasCardsLeft() {
-        return !deck.getCards().isEmpty();
+        return !game.getDeck().getCards().isEmpty();
     }
 
     @Override
@@ -76,9 +68,9 @@ public class GameServiceImpl implements GameService {
         // list to store all the cards in the game
         List<Card> allCards = new ArrayList<>();
         // add all cards in the deck to the list
-        allCards.addAll(deck.getCards());
+        allCards.addAll(game.getDeck().getCards());
         // add all cards on the table to the list
-        allCards.addAll(table);
+        allCards.addAll(game.getTable());
         // check all players' hands for duplicates
         for (Player player : game.getPlayers()) {
             // add all cards in the player's hand to the list
@@ -102,25 +94,25 @@ public class GameServiceImpl implements GameService {
     @Override
     public void addCardToTable(Card card) {
         // adding a card to the table
-        table.add(card);
+        game.getTable().add(card);
         logger.info(card.getRank() + " of " + card.getSuit() + " was placed on table.");
         logger.debug("Top card is the " + getLeadRank() + " of " + getLeadSuit());
         // Check if the deck has less than 4 cards remaining
-        if(deck.getCards().size() <= 3){
+        if(game.getDeck().getCards().size() <= 3){
             // removes every card but the lead card 
-            table.removeIf(tableCard -> !tableCard.equals(getLeadCard()));
+            game.getTable().removeIf(tableCard -> !tableCard.equals(getLeadCard()));
             // adds them back to deck 
-            deck.addAll(table);
+            game.getDeck().addAll(game.getTable());
             // clear the table 
-            table.clear();
+            game.getTable().clear();
             //shuffles the deck
-            deckService.shuffle(deck);
+            deckService.shuffle(game.getDeck());
         }
     }
 
     @Override
     public Card drawCard(Player player) {
-        Card card = deckService.deal(deck);
+        Card card = deckService.deal(game.getDeck());
         playerService.draw(player, card);
 
         logger.info(player + " drew a " + card.getRank() + " of " + card.getSuit());
@@ -135,7 +127,7 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public boolean isCardValid(Card card) {
-        return rulesService.isCardValid(card, getLeadSuit(), getLeadRank(), rules);
+        return rulesService.isCardValid(card, getLeadSuit(), getLeadRank(), game.getRules());
     }
 
     @Override
@@ -143,20 +135,22 @@ public class GameServiceImpl implements GameService {
         // check if game is in reverse order
         if (isReversed()) {
             // decrement current player by noOfTurns
-            currentPlayer -= noOfTurns;
+            game.setCurrentPlayer(noOfTurns-1);
         } else {
             // increment current player by noOfTurns
-            currentPlayer += noOfTurns;
+            game.setCurrentPlayer(noOfTurns+1);
+
 
         }
 
         // check if current player is less than 0
-        if (currentPlayer < 0) {
+        if (game.getCurrentPlayer() < 0) {
             // set current player to last player in the list
-            currentPlayer = game.getPlayers().size() - 1;
-        } else if (currentPlayer == game.getPlayers().size()) {
+            game.setCurrentPlayer(game.getPlayers().size() - 1);
+
+        } else if (game.getCurrentPlayer() == game.getPlayers().size()) {
             // set current player to first player in the list
-            currentPlayer = 0;
+            game.setCurrentPlayer(0);
         }
     }
 
@@ -168,17 +162,17 @@ public class GameServiceImpl implements GameService {
     @Override
     public int getCurrentPlayer() {
 
-        return currentPlayer;
+        return game.getCurrentPlayer();
     }
 
     @Override
     public Card.Suit getLeadSuit() {
-        return table.get(table.size() - 1).getSuit();
+        return game.getTable().get(game.getTable().size() - 1).getSuit();
     }
 
     @Override
     public Card.Rank getLeadRank() {
-        return table.get(table.size() - 1).getRank();
+        return game.getTable().get(game.getTable().size() - 1).getRank();
     }
 
     @Override
@@ -193,43 +187,43 @@ public class GameServiceImpl implements GameService {
 
     @Override
     public boolean isCardValid(Card card, Card lead) {
-        return rulesService.isCardValid(card, getLeadSuit(), getLeadRank(), rules);
+        return rulesService.isCardValid(card, getLeadSuit(), getLeadRank(), game.getRules());
     }
 
     @Override
     public boolean isDrawTwoOnSeven() {
-        return rules.isDrawTwoOnSeven();
+        return game.getRules().isDrawTwoOnSeven();
     }
 
     @Override
     public boolean isChooseSuitOnJack() {
-        return rules.isChooseSuitOnJack();
+        return game.getRules().isChooseSuitOnJack();
     }
 
     @Override
     public boolean isReverseOnAce() {
-        return rules.isReverseOnAce();
+        return game.getRules().isReverseOnAce();
     }
 
     @Override
     public boolean isReversed() {
-        return rules.isReversed();
+        return game.getRules().isReversed();
     }
 
     @Override
     public void setReversed(boolean reversed) {
-        rules.setReversed(reversed);
+        game.getRules().setReversed(reversed);
         ;
     }
 
     @Override
     public Deck getDeck() {
-        return deck;
+        return game.getDeck();
     }
 
     @Override
     public Rules getRules() {
-        return rules;
+        return game.getRules();
     }
 
     @Override
@@ -241,11 +235,11 @@ public class GameServiceImpl implements GameService {
     @Override
     public List<Card> getTable() {
 
-        return table;
+        return game.getTable();
     }
 
     @Override
     public void setSuitChoice(Card.Suit suit) {
-        rules.setSuit(suit);
+        game.getRules().setSuit(suit);
     }
 }
