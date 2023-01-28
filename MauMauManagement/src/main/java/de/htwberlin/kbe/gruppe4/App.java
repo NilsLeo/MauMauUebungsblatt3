04a5efc1fbe718410019d870c95045106be5805e
NilsLeo <nilstleo01@gmail.com>
2023-01-28 23:17:54 +0100

    refactored main to app.java

--- MauMauManagement/src/main/java/de/htwberlin/kbe/gruppe4/App.java
+++ MauMauManagement/src/main/java/de/htwberlin/kbe/gruppe4/App.java
@@ -1,4 +1,4 @@
-package de.htwberlin.kbe.gruppe4.impl;
+package de.htwberlin.kbe.gruppe4;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -6,7 +6,7 @@ import com.google.inject.Injector;
 import de.htwberlin.kbe.gruppe4.config.MauMauModule;
 import de.htwberlin.kbe.gruppe4.inter.CLIController;
 
-public class Main {
+public class App {
 
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new MauMauModule());
