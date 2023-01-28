cdb0dbf65bce53e957e236023b6f70971cd13a8f
NilsLeo <nilstleo01@gmail.com>
2023-01-28 21:57:01 +0100

    Maven correctiosn mostly implemenmted

--- DeckManagement/src/test/java/de/htwberlin/kbe/gruppe4/inter/DeckServiceImplTest.java
+++ DeckManagement/src/test/java/de/htwberlin/kbe/gruppe4/inter/DeckServiceImplTest.java
@@ -19,73 +19,78 @@ import de.htwberlin.kbe.gruppe4.impl.DeckServiceImpl;
 @ExtendWith(MockitoExtension.class)
 public class DeckServiceImplTest {
 
-    @Mock
-    private Deck deck;
-
-    @Mock
-    private Card card;
-
-    private DeckService deckService;
+    private DeckServiceImpl deckService;
 
     @BeforeEach
     public void setUp() {
         deckService = new DeckServiceImpl();
     }
+
     @Test
     public void testShuffle() {
-        // create a mock of the Deck interface
-        Deck mockDeck = Mockito.mock(Deck.class);
+        // create an instance of the Deck class
+        Deck deck = new Deck();
     
-        // set up the mock to return a specific list of cards when the getCards method is called
-        List<Card> cards = new ArrayList<>();
-        cards.add(new Card(Card.Suit.CLUBS, Card.Rank.ACE));
-        cards.add(new Card(Card.Suit.HEARTS, Card.Rank.SEVEN));
-        cards.add(new Card(Card.Suit.DIAMONDS, Card.Rank.EIGHT));
-        Mockito.when(mockDeck.getCards()).thenReturn(cards);
+        // create a copy of the original list of cards
+        List<Card> originalCards = new ArrayList<>(deck.getCards());
     
         // create an instance of the DeckServiceImpl class
         DeckServiceImpl deckService = new DeckServiceImpl();
     
-        // create a copy of the original list of cards
-        List<Card> originalCards = new ArrayList<>(cards);
-    
         // call the shuffle method of the deckService instance
-        deckService.shuffle(mockDeck);
+        deckService.shuffle(deck);
 
         // verifies that the 2 lists aren't equal, since they have a different order
-        assertNotEquals(mockDeck.getCards(), originalCards);
+        assertNotEquals(deck.getCards(), originalCards);
         // verifies that the 2 lists have the same elements regardless of order
-        assertTrue(() -> originalCards.containsAll(mockDeck.getCards()) && mockDeck.getCards().containsAll(originalCards));
+        assertTrue(() -> originalCards.containsAll(deck.getCards()) && deck.getCards().containsAll(originalCards));
     }
 
     @Test
     public void testDealHandSize() {
+    // create an instance of the Deck class
+    Deck deck = new Deck();
         // create an instance of the DeckServiceImpl class
         DeckServiceImpl deckService = new DeckServiceImpl();
-        
+    
+        // call the dealHand method of the deckService instance
         List<Card> hand = deckService.dealHand(deck);
         
+        // check that the size of the hand is equal to 5
         assertEquals(5, hand.size());
     }
-
+    
 
     @Test
     public void testDeal() {
+        // create an instance of the Deck class
+        Deck deck = new Deck();
+        
+        // set the cards of the deck
         List<Card> cards = new ArrayList<>();
         cards.add(new Card(Card.Suit.CLUBS, Card.Rank.ACE));
         cards.add(new Card(Card.Suit.HEARTS, Card.Rank.SEVEN));
         cards.add(new Card(Card.Suit.DIAMONDS, Card.Rank.EIGHT));
+        deck.setCards(cards);
 
-        when(deck.getCards()).thenReturn(cards);
+        // create an instance of the DeckServiceImpl class
+        DeckServiceImpl deckService = new DeckServiceImpl();
 
+        // get the first card from the deck
         Card card = cards.get(0);
-        Card dealtCard = deckService.deal(deck);
-        Card cardAfterDeal = cards.get(0);
 
+        // deal the first card
+        Card dealtCard = deckService.deal(deck);
+
+        // get the first card after deal
+        Card cardAfterDeal = cards.get(0);
 
         // Verify that the correct card was dealt
         assertEquals(card, dealtCard);
         assertNotEquals(cardAfterDeal, dealtCard);
     }
+
+
+
 }
 
--- MauMauManagement/src/main/java/de/htwberlin/kbe/gruppe4/config/GameModule.java
+++ MauMauManagement/src/main/java/de/htwberlin/kbe/gruppe4/config/GameModule.java
@@ -1,6 +1,10 @@
-package de.htwberlin.kbe.gruppe4.inter;
+package de.htwberlin.kbe.gruppe4.config;
 import com.google.inject.AbstractModule;
 import de.htwberlin.kbe.gruppe4.impl.*;
+import de.htwberlin.kbe.gruppe4.inter.CLIService;
+import de.htwberlin.kbe.gruppe4.inter.DeckService;
+import de.htwberlin.kbe.gruppe4.inter.PlayerService;
+import de.htwberlin.kbe.gruppe4.inter.RulesService;
 
 public class GameModule extends AbstractModule {
     @Override
